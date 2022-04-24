ef750f9aba93efa4cf09acf132ba405be8f9ca52
vladimirangarita <vladimir.angarita@gmail.com>
2022-04-24 19:29:42 -0400

    Capitulo 11 Metodos de generacion y Particionamiento

---
+++ EjemplosWeb/Generacion.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Generacion.aspx.cs" Inherits="EjemplosWeb.Generacion" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Larger" Text="Pruebas de Generacion y Particion"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Cuantos"></asp:Label>
+            <asp:TextBox ID="TxtCuantos" runat="server" style="margin-left: 74px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            Selecciona inicio<asp:DropDownList ID="CmbInicio" runat="server" AutoPostBack="True" OnSelectedIndexChanged="CmbInicio_SelectedIndexChanged" style="margin-left: 22px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:GridView ID="GrvDatos" runat="server" Width="900px">
+            </asp:GridView>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ EjemplosWeb/Generacion.aspx.cs
@@ -0,0 +1,40 @@
+﻿using EjemplosWin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjemplosWeb
+{
+    public partial class Generacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (BdSolicitudes Solicitudes = new BdSolicitudes())
+                {
+                    int Conteo = Solicitudes.Casos.Count();
+                    CmbInicio.DataSource = Enumerable.Range(1,Conteo);
+                    CmbInicio.DataBind();
+                }
+            }
+        }
+
+        protected void CmbInicio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TxtCuantos.Text))
+            {
+                using (BdSolicitudes Solicitudes = new BdSolicitudes())
+                {
+                    int Cuantos = Convert.ToInt32(TxtCuantos.Text);
+                    int Inicio = Convert.ToInt32(CmbInicio.SelectedItem.Text);
+                    GrvDatos.DataSource = Solicitudes.Casos.Skip(Inicio-1).Take(Cuantos);
+                    GrvDatos.DataBind();
+                }
+            }
+        }
+    }
+}
